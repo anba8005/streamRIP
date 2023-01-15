214e7c4387b214be63ea9942673c8b461f46d1bd
Andrejus <anba8005@gmail.com>
2023-01-15 20:43:26 +0200

    detach container by default

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 docker stop streamrip-$1
-docker run --rm --name streamrip-$1 --mount type=tmpfs,destination=/mnt/hls -p $1:1935 -v $2:/media -it streamrip
+docker run -d --rm --name streamrip-$1 --mount type=tmpfs,destination=/mnt/hls -p $1:1935 -v $2:/media -it streamrip
